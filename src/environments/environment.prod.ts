f1fcf34933a3c04af2eea9ee54fce3720c476a05
Nelson Seixas <nsouzarj@outlook.com>
2025-09-20 13:43:15 -0300

    Feito

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:'http://192.168.1.105:8081/cra-api' 
+  apiUrl: 'https://cra-backend-nnb0.onrender.com/cra-api'  // Production API URL
 };
