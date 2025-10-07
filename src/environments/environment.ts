f01cf249b1b674b635355af8355abbabddf649b9
Nelson Seixas <nsouzarj@outlook.com>
2025-10-07 17:17:27 -0300

    feito

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/cra-api'  // Development API URL
+  apiUrl: 'http://192.168.1.106:8081/cra-api'  // Development API URL
 };
 
 /*
