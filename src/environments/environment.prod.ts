3af76aa24ab39baf26faf09f767c41722782a533
Nelson Seixas <nsouzarj@outlook.com>
2025-10-19 08:08:59 -0300

    Ajuste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cra-backend-rf76.onrender.com/cra-api'  // Production API URL
+  apiUrl: 'http://192.168.1.107:8081/cra-api'  // Production API URL
 
 };
